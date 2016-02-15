bbb82519997a9014e66b032d023933e3859b2bfb
Paul Bardea <paul@pbardea.com>
2016-02-15 00:56:18 -0500

    First commit

---
+++ KeepIt/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  KeepIt
+//
+//  Created by Paul Bardea on 2016-02-14.
+//  Copyright © 2016 pbardea stdios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
