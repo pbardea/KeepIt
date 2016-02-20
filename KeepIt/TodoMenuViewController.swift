4f6cb6aec573b8c7d780845508e98417d1ae50a3
Paul Bardea <paul@pbardea.com>
2016-02-20 11:58:51 -0500

    Made some progress with xib files

--- KeepIt/TodoMenuViewController.swift
+++ KeepIt/TodoMenuViewController.swift
@@ -8,11 +8,51 @@
 
 import Cocoa
 
+class Todo {
+    var title: String
+    var completed = false
+    var dueDate: NSDate?
+    
+    init(title: String) {
+        self.title = title
+    }
+    
+    init(title: String, dueDate: NSDate) {
+        self.title = title
+        self.dueDate = dueDate
+    }
+}
+
 class TodoMenuViewController: NSViewController {
+    
+    var todoList = [Todo(title: "Thing 1"), Todo(title: "Thing 2")]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
     }
     
+}
+
+// MARK: - NSTableViewDataSource
+extension TodoMenuViewController: NSTableViewDataSource {
+    func numberOfRowsInTableView(aTableView: NSTableView) -> Int {
+        return self.todoList.count
+    }
+    
+    func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let cellView: NSTableCellView = tableView.makeViewWithIdentifier(tableColumn!.identifier, owner: self) as! NSTableCellView
+        
+        if tableColumn!.identifier == "todoListColumn" {
+            let todoItem = self.todoList[row]
+            cellView.backgroundStyle = NSBackgroundStyle.Light
+            return cellView
+        }
+        
+        return cellView
+    }
+}
+
+// MARK: - NSTableViewDelegate
+extension TodoMenuViewController: NSTableViewDelegate {
 }
