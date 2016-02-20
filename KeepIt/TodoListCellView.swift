db79d8261a872e68fe8f35f0fd1acd21b2756dec
Paul Bardea <paul@pbardea.com>
2016-02-20 14:42:28 -0500

    Made progress

---
+++ KeepIt/TodoListCellView.swift
@@ -0,0 +1,19 @@
+//
+//  TodoListCellView.swift
+//  KeepIt
+//
+//  Created by Paul Bardea on 2016-02-20.
+//  Copyright © 2016 pbardea stdios. All rights reserved.
+//
+
+import Cocoa
+
+class TodoListCellView: NSTableCellView {
+
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
