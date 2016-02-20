c982e63d6985e6e47974ee6350a87373fd6b2bdf
Paul Bardea <paul@pbardea.com>
2016-02-19 22:02:42 -0500

    Progres...

--- KeepIt/StatusMenuController.swift
+++ KeepIt/StatusMenuController.swift
@@ -11,15 +11,41 @@ import WebKit
 
 class StatusMenuController: NSObject, WKNavigationDelegate {
     
+    let popover = NSPopover()
+    
     @IBOutlet weak var statusMenu: NSMenu!
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(20)
     
     override func awakeFromNib() {
-        let icon = NSImage(named: "Todo")
-        icon?.size = NSSize(width: 15, height: 15)
-        icon?.template = true // best for dark mode
-        statusItem.image = icon
+        if let button = statusItem.button {
+            button.image = NSImage(named: "Todo")
+            button.image?.size = NSSize(width: 15, height: 15)
+            button.image?.template = true // best for dark mode
+        }
+        
         statusItem.menu = statusMenu
-    }    
+        statusItem.target = self
+        statusItem.action = Selector("togglePopover:")
+        
+        popover.contentViewController = TodoMenuViewController(nibName: "TodoMenuViewController", bundle: nil)
+    }
+    
+    func showPopover(sender: AnyObject?) {
+        if let button = statusItem.button {
+            popover.showRelativeToRect(button.bounds, ofView: button, preferredEdge: NSRectEdge.MinY)
+        }
+    }
+    
+    func closePopover(sender: AnyObject?) {
+        popover.performClose(sender)
+    }
+    
+    func togglePopover(sender: AnyObject?) {
+        if popover.shown {
+            closePopover(sender)
+        } else {
+            showPopover(sender)
+        }
+    }
 
 }
