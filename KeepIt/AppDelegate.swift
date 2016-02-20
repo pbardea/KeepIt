9d5f92de15331120d036ae53d2cf37a77b4e27ab
Paul Bardea <paul@pbardea.com>
2016-02-19 23:15:54 -0500

    Popover working

--- KeepIt/AppDelegate.swift
+++ KeepIt/AppDelegate.swift
@@ -10,15 +10,42 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSSquareStatusItemLength)
+    let popover = NSPopover()
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named: "Todo")
+            button.image?.template = true // best for dark mode
+            button.image?.size = NSSize(width: 15, height: 15)
+            button.action = Selector("togglePopover:")
+        }
+        
+        popover.contentViewController = TodoMenuViewController(nibName: "TodoMenuViewController", bundle: nil)
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
 
-
+    func showPopover(sender: AnyObject?) {
+        if let button = statusItem.button {
+            popover.showRelativeToRect(button.bounds, ofView: button, preferredEdge: NSRectEdge.MinY)
+        }
+    }
+    
+    func closePopover(sender: AnyObject?) {
+        popover.performClose(sender)
+    }
+    
+    func togglePopover(sender: AnyObject?) {
+        if popover.shown {
+            closePopover(sender)
+        } else {
+            showPopover(sender)
+        }
+    }
 }
 
