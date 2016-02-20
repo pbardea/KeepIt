72ef17f09c6ed12048cd885efcd3ec6ba84abdae
Paul Bardea <paul@pbardea.com>
2016-02-19 21:51:17 -0500

    Remove web view

--- KeepIt/StatusMenuController.swift
+++ KeepIt/StatusMenuController.swift
@@ -14,25 +14,12 @@ class StatusMenuController: NSObject, WKNavigationDelegate {
     @IBOutlet weak var statusMenu: NSMenu!
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(20)
     
-    let webView = WebView(frame: NSRect(x: 0, y: 0, width: 400, height: 300))
-    
     override func awakeFromNib() {
         let icon = NSImage(named: "Todo")
         icon?.size = NSSize(width: 15, height: 15)
         icon?.template = true // best for dark mode
         statusItem.image = icon
         statusItem.menu = statusMenu
-        
-        webView.mainFrameURL = "https://keep.google.com"
-        
-        let webItem = NSMenuItem(title: "testing", action: "reloadPressed:", keyEquivalent: "")
-        webItem.view = webView
-       
-        statusItem.menu?.addItem(webItem)
-    }
-    
-    func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation) {
-        print("Finished navigating to url")
-    }
+    }    
 
 }
